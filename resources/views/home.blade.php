fe1437da8657a46da372420aa2f4fa2c9269ec45
Rok Zabukovec <kzabuk@gmail.com>
2019-01-02 16:15:59 +0100

    added gitignore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,52 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
+    <div class="dashboard">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
 
-                    <div class="card-header">Dashboard</div>
-                    <div class="card-body flex">
-                        <div class="avatar-wrapper">
-                            <img class="avatar-img" src="{{$user->avatar}}" alt="avatar">
-                            <p class="avatar-img--change">Change avatar</p>
-                        </div>
-
-                        <div class="user-info">
-                            <p>{{ $user->name }}</p>
-                            <p>{{ $user->birthday }}</p>
-                            <p>{{ $user->email }}</p>
-                        </div>
-                        <div class="user-accounts">
-                            <div class="flex flex-title">
-                                <h2>Accounts</h2>
-                                <a class="button" href="{{ route('allAccounts') }}">New account</a>
+                        <div class="card-header">Dashboard</div>
+                        <div class="card-body flex">
+                            <div class="avatar-wrapper">
+                                <img class="avatar-img" src="{{$user->avatar}}" alt="avatar">
+                                <p class="avatar-img--change">Change avatar</p>
                             </div>
-                            @if(count($user->accounts) > 0 )
-                                <ul>
-                                    @foreach($user->accounts as $acc)
-                                        <a href="/home/accounts/{{$acc->account_id}}"> <li>{{$acc->type}} <span>{{$acc->balance}} €</span></li></a>
-                                    @endforeach
-                                </ul>
-                            @else
-                                <p>No accounts registered</p>
-                            @endif
+
+                            <div class="user-info">
+                                <p>{{ $user->name }}</p>
+                                <p>{{ $user->birthday }}</p>
+                                <p>{{ $user->email }}</p>
+                            </div>
+                            <div class="user-accounts">
+                                <div class="flex flex-title">
+                                    <h2>Accounts</h2>
+                                    <a href="{{ route('allAccounts') }}"><i class="material-icons large">
+                                            add_circle_outline
+                                        </i></a>
+                                </div>
+                                @if(count($user->accounts) > 0 )
+                                    <ul>
+                                        @foreach($user->accounts as $acc)
+                                            <a href="/home/accounts/{{$acc->account_id}}">
+                                                <li>{{$acc->type}} <span>{{$acc->balance}} €</span></li>
+                                            </a>
+                                        @endforeach
+                                    </ul>
+                                @else
+                                    <p>No accounts registered</p>
+                                @endif
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="avatar-modal">
+            <h3>Change avatar</h3>
+            <form action="{{ route('avatarUpdate') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <input type="file" name="avatar" accept="image/*">
+                    <input class="btn btn-block btn-success margin-top-25" type="submit" value="Change avatar">
+                </div>
+            </form>
+            <p class="close">x</p>
+        </div>
     </div>
-    <div class="avatar-modal">
-        <h3>Change avatar</h3>
-        <form action="{{ route('avatarUpdate') }}" method="POST"  enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <input type="file" name="avatar" accept="image/*">
-                <input class="btn btn-block btn-success margin-top-25" type="submit" value="Change avatar">
-            </div>
-        </form>
-        <p class="close">x</p>
-    </div>
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content">
+
         <div class="hero">
             <h1> {{env('APP_NAME')}} </h1>
             <p>Take your finance in your own hands.</p>
         </div>
-    </div>
+    <section class="wise-description flex flex-column" style="justify-content: center; align-items: center; padding">
+        <div class="wise-text" style="width: 50%;">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores consequuntur deserunt nihil,
+                numquam ullam veniam? Adipisci architecto deleniti dicta dolore ea eaque earum ex iste maiores molestias
+                mollitia nihil nobis officia perspiciatis provident quibusdam, saepe, totam ut? Aspernatur assumenda id in
+                incidunt mollitia nam perspiciatis. Blanditiis dolor ipsum labore.</p>
+            <button class="btn btn-warning">REGISTER</button>
+        </div>
+    </section>
 @endsection
